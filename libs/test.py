6638c79bceb21b8e0cdf8a87936a62a31659e7d4
lck898989 <1783887127@qq.com>
2019-12-27 21:58:01 +0800

    add some lib

---
+++ libs/test.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+print("os is ",os);
+print("path is ",Path);
+print("path is ",pathlib);
+print("cwd is ",pathlib.Path.cwd);
+if __name__ == "__main__":
+    print("进入main");
